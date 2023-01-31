a2cf5eb615c8d7717af0f8d8dd57b122904e2a29
nunesxd <55452280+nunesxd@users.noreply.github.com>
2023-01-31 15:28:16 -0300

    Criação do evento de deleção dos items

--- src/App.js
+++ src/App.js
@@ -8,11 +8,17 @@ import FeedbackData from './data/FeedbackData';
 function App() {
     const [feedback, setFeedback] = useState( FeedbackData );
 
+    const deleteFeedback = (id) => {
+        if(window.confirm('Are you sure you want do delete this item ?')) {
+            setFeedback(feedback.filter( (item) => item.id !== id ));
+        }
+    }
+
     return (
         <>
             <Header text="Prop text testing..." />
             <div className='container'>
-                <FeedbackList feedback={feedback} />
+                <FeedbackList feedback={feedback} handleDelete={ deleteFeedback } />
             </div>
         </>
     )
--- src/components/FeedbackItem.jsx
+++ src/components/FeedbackItem.jsx
@@ -1,9 +1,10 @@
 import React, {useState} from 'react'
+import {FaTimes} from 'react-icons/fa'
 import PropTypes from 'prop-types'
 import Card from './shared/Card'
 
 
-function FeedbackItem({item}) {
+function FeedbackItem({item, handleDelete}) {
     // const [rating, setRating] = useState(7);
     // const [text, setText] = useState('This is an example of a feedback item');
 
@@ -18,6 +19,9 @@ function FeedbackItem({item}) {
     return (
     <Card>
         <div className="num-display">{ item.rating }</div>
+        <button onClick={() => handleDelete(item.id) } className="close">
+            <FaTimes color='purple' />
+        </button>
         <div className="text-display">{ item.text }</div>
         {/* <button onClick={handleClick}>Click me !</button> */}
     </Card>
