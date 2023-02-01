2b7a67ece2087546cba77b70c3913a36adc610ce
nunesxd <55452280+nunesxd@users.noreply.github.com>
2023-01-31 21:31:41 -0300

    Criação do arquivo FeedbackForm

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import Header from './components/Header';
 //import FeedbackItem from './components/FeedbackItem';
 import FeedbackList from './components/FeedbackList';
 import FeedbackStats from './components/FeedbackStats';
+import FeedbackForm from './components/FeedbackForm';
 import FeedbackData from './data/FeedbackData';
 
 
@@ -19,6 +20,7 @@ function App() {
         <>
             <Header text="Prop text testing..." />
             <div className='container'>
+                <FeedbackForm />
                 <FeedbackStats feedback={feedback} />
                 <FeedbackList feedback={feedback} handleDelete={ deleteFeedback } />
             </div>
---
+++ src/components/FeedbackForm.jsx
@@ -0,0 +1,23 @@
+import React, { useState } from 'react';
+import Card from './shared/Card'
+
+function FeedbackForm() {
+    const [text, setText] = useState( '' );
+
+    const handleTextChange = (event) => {setText(event.target.value);};
+
+    return (
+        <Card>
+            <form>
+                <h4>How would you rate this site ?</h4>
+
+                <div className="input-group">
+                    <input onChange={ handleTextChange } type="text" placeholder='Write a review' value={ text } />
+                    <button type="submit">Send</button>
+                </div>
+            </form>
+        </Card>
+    )
+}
+
+export default FeedbackForm
