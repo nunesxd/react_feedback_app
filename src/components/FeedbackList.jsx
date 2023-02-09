9dad49a379257b0aa162a28721c62f2fb840f87a
nunesxd <55452280+nunesxd@users.noreply.github.com>
2023-02-09 16:16:50 -0300

    Implementação do gi de loading

--- src/components/FeedbackList.jsx
+++ src/components/FeedbackList.jsx
@@ -2,26 +2,27 @@ import { useContext, React } from 'react'
 import FeedbackItem from './FeedbackItem'
 // import PropTypes from 'prop-types'
 import FeedbackContext from '../context/FeedbackContext'
+import Spinner from './shared/Spinner'
 
 
 // Função antiga, quando passavamos os argumentos por 'props':
 // function FeedbackList({feedback, handleDelete}) {
 function FeedbackList() {
-    const {feedback} = useContext(FeedbackContext);
+    const {feedback, isLoading} = useContext(FeedbackContext);
     
-    if(!feedback || feedback.length === 0) {
+    if(!isLoading && (!feedback || feedback.length === 0)) {
         return <p>No Feedback received</p>
     }
 
-    return (
-        <div className='feedback-list'>
-            {feedback.map( (item) => (
-                <FeedbackItem key={item.key} item={item} />
-            ) )}
-        </div>
-        // Com o context gerado, agora iremos realizar os procedimentos de CRUD diretamente pelo item.
-        // handleDelete={ handleDelete }
-    )
+    return isLoading ? (<Spinner />) : (
+            <div className='feedback-list'>
+                {feedback.map( (item) => (
+                    <FeedbackItem key={item.key} item={item} />
+                ) )}
+            </div>
+            // Com o context gerado, agora iremos realizar os procedimentos de CRUD diretamente pelo item.
+            // handleDelete={ handleDelete }
+        )
 }
 
 // Não é mais necessário, visto que agora estamos usando o conceito de 'context':
